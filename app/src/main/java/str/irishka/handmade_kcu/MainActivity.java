e20a556165c3c3e97705df2f8f8e4c44a2dfebe1
irishka-str <irishka.str@list.ru>
2015-10-10 15:07:41 +0400

    Добавлен главный активити

---
+++ app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
@@ -0,0 +1,19 @@
+package str.irishka.handmade_kcu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    private static final int LAYOUT = R.layout.activity_main;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(LAYOUT);
+    }
+
+}
