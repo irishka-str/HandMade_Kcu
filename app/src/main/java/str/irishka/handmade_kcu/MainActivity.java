d667990da357e6828f092862e929f31705c5a43b
irishka-str <irishka.str@list.ru>
2015-10-12 00:12:23 +0400

    Добавлен ToolBar

--- app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
+++ app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
@@ -4,16 +4,33 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
 
     private static final int LAYOUT = R.layout.activity_main;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(LAYOUT);
+
+        initToolbar();
     }
 
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
+    }
 }
