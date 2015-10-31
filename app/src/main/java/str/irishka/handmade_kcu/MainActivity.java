ffef12875e37652974e6f08944bd998d1d7ed7aa
irishka-str <irishka.str@list.ru>
2015-11-01 01:15:59 +0400

    ///

--- app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
+++ app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
@@ -2,13 +2,16 @@ package str.irishka.handmade_kcu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.Window;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity  {
 
     private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
@@ -16,6 +19,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(LAYOUT);
 
         initToolbar();
@@ -23,7 +27,8 @@ public class MainActivity extends Activity {
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(R.string.app_name);
+       // toolbar.setTitle(R.string.app_name);
+        toolbar.setTitle("toolbar");
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
