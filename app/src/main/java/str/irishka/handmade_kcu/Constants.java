4fbc7546f8aa957e0111d309dd5d26e4829d67ea
irishka-str <irishka.str@list.ru>
2015-11-29 22:51:32 +0400

    сделан NavigationView, NavigationTabs переход по табам из меню справа добавлен один тестовый фрагмент, для табов

---
+++ app/src/main/java/str/irishka/handmade_kcu/Constants.java
@@ -0,0 +1,12 @@
+package str.irishka.handmade_kcu;
+
+/**
+ * Created by user on 29.11.2015.
+ */
+public class Constants {
+
+    public static final int TAB_ONE = 0;
+    public static final int TAB_TWO = 1;
+    public static final int TAB_THREE = 2;
+
+}
--- app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
+++ app/src/main/java/str/irishka/handmade_kcu/MainActivity.java
@@ -2,34 +2,39 @@ package str.irishka.handmade_kcu;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.Window;
 
+import str.irishka.handmade_kcu.adapter.TabPagerFragmentAdapter;
 
 public class MainActivity extends AppCompatActivity  {
 
     private static final int LAYOUT = R.layout.activity_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(LAYOUT);
 
         initToolbar();
+        initNavigationView();
+        initTabs();
     }
 
     private void initToolbar() {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-       // toolbar.setTitle(R.string.app_name);
-        toolbar.setTitle("toolbar");
+        toolbar.setTitle(R.string.app_name);
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
@@ -38,5 +43,43 @@ public class MainActivity extends AppCompatActivity  {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                                                                 R.string.view_navigation_open,
+                                                                 R.string.view_navigation_close);
+
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch(menuItem.getItemId()) {
+                    case R.id.actionNotificationItem:
+                        showNotificationTabs();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabPagerFragmentAdapter adapter = new TabPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    public void showNotificationTabs() {
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 }
